7252d8d5663b3896fcbd533ff94050202850ff34
LayNath242 <laynath242@gmail.com>
2022-03-29 14:48:26 +0700

    add feed pallets

---
+++ pallets/pallet-feeds/src/lib.rs
@@ -0,0 +1,182 @@
+// Copyright (C) 2021 Subspace Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// 	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//! Pallet feeds, used for storing arbitrary user-provided data combined into feeds.
+
+#![cfg_attr(not(feature = "std"), no_std)]
+#![forbid(unsafe_code)]
+#![warn(rust_2018_idioms, missing_debug_implementations)]
+
+use core::mem;
+pub use pallet::*;
+use kumandra_core_primitives::{crypto, Sha256Hash};
+
+#[cfg(all(feature = "std", test))]
+mod mock;
+#[cfg(all(feature = "std", test))]
+mod tests;
+
+#[frame_support::pallet]
+mod pallet {
+    use frame_support::pallet_prelude::*;
+    use frame_system::pallet_prelude::*;
+    use sp_std::prelude::*;
+
+    #[pallet::config]
+    pub trait Config: frame_system::Config {
+        /// `pallet-feeds` events
+        type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
+    }
+
+    /// Pallet feeds, used for storing arbitrary user-provided data combined into feeds.
+    #[pallet::pallet]
+    #[pallet::generate_store(pub(super) trait Store)]
+    #[pallet::without_storage_info]
+    pub struct Pallet<T>(_);
+
+    /// User-provided object to store
+    pub(super) type Object = Vec<u8>;
+    /// ID of the feed
+    pub(super) type FeedId = u64;
+    /// User-provided object metadata (not addressable directly, but available in an even)
+    pub(super) type ObjectMetadata = Vec<u8>;
+
+    /// Total amount of data and number of objects stored in a feed
+    #[derive(Debug, Decode, Encode, TypeInfo, Default, PartialEq, Eq)]
+    pub struct TotalObjectsAndSize {
+        /// Total size of objects in bytes
+        pub size: u64,
+        /// Total number of objects
+        pub count: u64,
+    }
+
+    #[pallet::storage]
+    #[pallet::getter(fn metadata)]
+    pub(super) type Metadata<T: Config> =
+        StorageMap<_, Blake2_128Concat, FeedId, ObjectMetadata, OptionQuery>;
+
+    #[pallet::storage]
+    #[pallet::getter(fn totals)]
+    pub(super) type Totals<T: Config> =
+        StorageMap<_, Blake2_128Concat, FeedId, TotalObjectsAndSize, ValueQuery>;
+
+    #[pallet::storage]
+    #[pallet::getter(fn current_feed_id)]
+    pub(super) type CurrentFeedId<T: Config> = StorageValue<_, FeedId, ValueQuery>;
+
+    /// `pallet-feeds` events
+    #[pallet::event]
+    #[pallet::generate_deposit(pub(super) fn deposit_event)]
+    pub enum Event<T: Config> {
+        /// New object was added.
+        ObjectSubmitted {
+            metadata: ObjectMetadata,
+            who: T::AccountId,
+            object_size: u64,
+        },
+        /// New feed was created.
+        FeedCreated { feed_id: FeedId, who: T::AccountId },
+    }
+
+    /// `pallet-feeds` errors
+    #[pallet::error]
+    pub enum Error<T> {
+        /// `FeedId` doesn't exist
+        UnknownFeedId,
+    }
+
+    #[pallet::call]
+    impl<T: Config> Pallet<T> {
+        // TODO: add proper weights
+        /// Create a new feed
+        #[pallet::weight(10_000)]
+        pub fn create(origin: OriginFor<T>) -> DispatchResult {
+            let who = ensure_signed(origin)?;
+
+            let feed_id = Self::current_feed_id();
+
+            CurrentFeedId::<T>::mutate(|feed_id| *feed_id = feed_id.saturating_add(1));
+
+            Totals::<T>::insert(feed_id, TotalObjectsAndSize::default());
+
+            Self::deposit_event(Event::FeedCreated { feed_id, who });
+
+            Ok(())
+        }
+
+        // TODO: add proper weights
+        // TODO: For now we don't have fees, but we will have them in the future
+        /// Put a new object into a feed
+        #[pallet::weight((10_000, Pays::No))]
+        pub fn put(
+            origin: OriginFor<T>,
+            feed_id: FeedId,
+            object: Object,
+            metadata: ObjectMetadata,
+        ) -> DispatchResult {
+            let who = ensure_signed(origin)?;
+
+            let object_size = object.len() as u64;
+
+            log::debug!("metadata: {:?}", metadata);
+            log::debug!("object_size: {:?}", object_size);
+
+            let current_feed_id = Self::current_feed_id();
+
+            ensure!(current_feed_id >= feed_id, Error::<T>::UnknownFeedId);
+
+            Metadata::<T>::insert(feed_id, metadata.clone());
+
+            Totals::<T>::mutate(feed_id, |feed_totals| {
+                feed_totals.size += object_size;
+                feed_totals.count += 1;
+            });
+
+            Self::deposit_event(Event::ObjectSubmitted {
+                metadata,
+                who,
+                object_size,
+            });
+
+            Ok(())
+        }
+    }
+}
+
+/// Mapping to the object offset and size within an extrinsic
+#[derive(Debug)]
+pub struct CallObject {
+    /// Object hash
+    pub hash: Sha256Hash,
+    /// Offset of object in the encoded call.
+    pub offset: u32,
+}
+
+impl<T: Config> Call<T> {
+    /// Extract the call object if an extrinsic corresponds to `put` call
+    pub fn extract_call_object(&self) -> Option<CallObject> {
+        match self {
+            Self::put { object, .. } => {
+                // `FeedId` is the first field in the extrinsic. `1+` corresponds to `Call::put {}`
+                // enum variant encoding.
+                Some(CallObject {
+                    hash: crypto::sha256_hash(object),
+                    offset: 1 + mem::size_of::<FeedId>() as u32,
+                })
+            }
+            _ => None,
+        }
+    }
+}
---
+++ pallets/pallet-feeds/src/tests.rs
@@ -0,0 +1,82 @@
+use crate::mock::{new_test_ext, Event, Feeds, Origin, System, Test};
+use crate::{Error, FeedId, Object, ObjectMetadata, TotalObjectsAndSize};
+use frame_support::{assert_noop, assert_ok};
+
+const FEED_ID: FeedId = 0;
+const ACCOUNT_ID: u64 = 100;
+
+#[test]
+fn can_create_feed() {
+    new_test_ext().execute_with(|| {
+        // current feed id is 0 by default
+        assert_eq!(Feeds::current_feed_id(), FEED_ID);
+        assert_ok!(Feeds::create(Origin::signed(ACCOUNT_ID)));
+        // current feed id value should be incremented after feed is created
+        assert_eq!(Feeds::current_feed_id(), 1);
+
+        assert_eq!(Feeds::totals(0), TotalObjectsAndSize::default());
+
+        System::assert_last_event(Event::Feeds(crate::Event::<Test>::FeedCreated {
+            feed_id: FEED_ID,
+            who: ACCOUNT_ID,
+        }));
+    });
+}
+
+#[test]
+fn can_do_put() {
+    new_test_ext().execute_with(|| {
+        let object: Object = vec![1, 2, 3, 4, 5];
+        let object_size = object.len() as u64;
+        let object_metadata: ObjectMetadata = vec![6, 7, 8, 9, 10];
+        // create feed before putting any data
+        assert_eq!(Feeds::current_feed_id(), FEED_ID);
+
+        assert_ok!(Feeds::put(
+            Origin::signed(ACCOUNT_ID),
+            FEED_ID,
+            object,
+            object_metadata.clone()
+        ));
+
+        // check Metadata hashmap for updated metadata
+        assert_eq!(Feeds::metadata(FEED_ID), Some(object_metadata.clone()));
+
+        // check Totals hashmap
+        assert_eq!(
+            Feeds::totals(FEED_ID),
+            TotalObjectsAndSize {
+                count: 1,
+                size: object_size,
+            }
+        );
+
+        System::assert_last_event(Event::Feeds(crate::Event::<Test>::ObjectSubmitted {
+            metadata: object_metadata,
+            who: ACCOUNT_ID,
+            object_size,
+        }));
+    });
+}
+
+#[test]
+fn cannot_do_put_with_wrong_feed_id() {
+    new_test_ext().execute_with(|| {
+        // don't care about actual data and metadata, because call is supposed to fail
+        let object: Object = Object::default();
+        let object_metadata: ObjectMetadata = ObjectMetadata::default();
+        let wrong_feed_id = 178;
+
+        assert_noop!(
+            Feeds::put(
+                Origin::signed(ACCOUNT_ID),
+                wrong_feed_id,
+                object,
+                object_metadata
+            ),
+            Error::<Test>::UnknownFeedId
+        );
+
+        assert_eq!(System::events().len(), 0);
+    });
+}
