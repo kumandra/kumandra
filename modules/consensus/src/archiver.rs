c3d84cf4fed86349c260c1a3432cef4ae9024475
Hangsia Hong <44630728+hangsiahong@users.noreply.github.com>
2022-04-05 09:00:46 +0100

    add Kumandra consensus modules. based on a folk of sc_consensus_babe

---
+++ modules/consensus/src/archiver.rs
@@ -0,0 +1,302 @@
+// Copyright (C) 2022 Kumandra, Inc.
+// SPDX-License-Identifier: GPL-3.0-or-later WITH Classpath-exception-2.0
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+use crate::KumandraLink;
+use codec::Encode;
+use futures::{SinkExt, StreamExt};
+use log::{debug, error, info};
+use sc_client_api::BlockBackend;
+use sp_api::ProvideRuntimeApi;
+use sp_blockchain::HeaderBackend;
+use kp_consensus::KumandraApi;
+use sp_runtime::generic::BlockId;
+use sp_runtime::traits::{Block as BlockT, CheckedSub, Header, One, Saturating, Zero};
+use std::sync::Arc;
+use kumandra_archiving::archiver::{ArchivedSegment, Archiver};
+use kumandra_core_primitives::{BlockNumber, RootBlock};
+
+fn find_last_root_block<Block: BlockT, Client>(client: &Client) -> Option<RootBlock>
+where
+    Client: ProvideRuntimeApi<Block> + BlockBackend<Block> + HeaderBackend<Block>,
+    Client::Api: KumandraApi<Block>,
+{
+    let mut block_to_check = BlockId::Hash(client.info().best_hash);
+    loop {
+        let block = client
+            .block(&block_to_check)
+            .expect("Older blocks should always exist")
+            .expect("Older blocks should always exist");
+
+        for extrinsic in block.block.extrinsics() {
+            match client
+                .runtime_api()
+                .extract_root_blocks(&block_to_check, extrinsic)
+            {
+                Ok(Some(root_blocks)) => {
+                    return root_blocks.into_iter().last();
+                }
+                Ok(None) => {
+                    // Some other extrinsic, ignore
+                }
+                Err(error) => {
+                    // TODO: Probably light client, can this even happen?
+                    error!(target: "kumandra", "Failed to make runtime API call: {:?}", error);
+                }
+            }
+        }
+
+        let parent_block_hash = *block.block.header().parent_hash();
+
+        if parent_block_hash == Block::Hash::default() {
+            // Genesis block, nothing else to check
+            break None;
+        }
+
+        block_to_check = BlockId::Hash(parent_block_hash);
+    }
+}
+
+/// Start an archiver that will listen for imported blocks and archive blocks at `K` depth,
+/// producing pieces and root blocks (root blocks are then added back to the blockchain as
+/// `store_root_block` extrinsic).
+pub fn start_kumandra_archiver<Block: BlockT, Client>(
+    kumandra_link: &KumandraLink<Block>,
+    client: Arc<Client>,
+    spawner: &impl sp_core::traits::SpawnEssentialNamed,
+) where
+    Client: ProvideRuntimeApi<Block>
+        + BlockBackend<Block>
+        + HeaderBackend<Block>
+        + Send
+        + Sync
+        + 'static,
+    Client::Api: KumandraApi<Block>,
+{
+    let best_block_id = BlockId::Hash(client.info().best_hash);
+
+    let confirmation_depth_k = TryInto::<BlockNumber>::try_into(
+        client
+            .runtime_api()
+            .confirmation_depth_k(&best_block_id)
+            .expect("Failed to get `confirmation_depth_k` from runtime API"),
+    )
+    .unwrap_or_else(|_| {
+        panic!("Confirmation depth K can't be converted into BlockNumber");
+    });
+    let record_size = client
+        .runtime_api()
+        .record_size(&best_block_id)
+        .expect("Failed to get `record_size` from runtime API");
+    let recorded_history_segment_size = client
+        .runtime_api()
+        .recorded_history_segment_size(&best_block_id)
+        .expect("Failed to get `recorded_history_segment_size` from runtime API");
+
+    let maybe_last_root_block = find_last_root_block(client.as_ref());
+
+    let mut archiver = if let Some(last_root_block) = maybe_last_root_block {
+        // Continuing from existing initial state
+        let last_archived_block_number = last_root_block.last_archived_block().number;
+        info!(
+            target: "kumandra",
+            "Last archived block {}",
+            last_archived_block_number,
+        );
+        let last_archived_block = client
+            .block(&BlockId::Number(last_archived_block_number.into()))
+            .expect("Older blocks must always exist")
+            .expect("Older blocks must always exist");
+
+        let block_object_mapping = client
+            .runtime_api()
+            .extract_block_object_mapping(
+                &BlockId::Number(last_archived_block_number.saturating_sub(1).into()),
+                last_archived_block.block.clone(),
+            )
+            .expect("Must be able to make runtime call");
+
+        Archiver::with_initial_state(
+            record_size as usize,
+            recorded_history_segment_size as usize,
+            last_root_block,
+            &last_archived_block.encode(),
+            block_object_mapping,
+        )
+        .expect("Incorrect parameters for archiver")
+    } else {
+        info!(target: "kumandra", "Starting archiving from genesis");
+
+        Archiver::new(record_size as usize, recorded_history_segment_size as usize)
+            .expect("Incorrect parameters for archiver")
+    };
+
+    // Process blocks since last fully archived block (or genesis) up to the current head minus K
+    {
+        let blocks_to_archive_from = archiver
+            .last_archived_block_number()
+            .map(|n| n + 1)
+            .unwrap_or_default();
+        let best_number = client.info().best_number;
+        let blocks_to_archive_to = TryInto::<BlockNumber>::try_into(best_number)
+            .unwrap_or_else(|_| {
+                panic!(
+                    "Best block number {} can't be converted into BlockNumber",
+                    best_number,
+                );
+            })
+            .checked_sub(confirmation_depth_k)
+            .or_else(|| {
+                if maybe_last_root_block.is_none() {
+                    // If not continuation, archive genesis block
+                    Some(0)
+                } else {
+                    None
+                }
+            });
+
+        if let Some(blocks_to_archive_to) = blocks_to_archive_to {
+            info!(
+                target: "kumandra",
+                "Archiving already produced blocks {}..={}",
+                blocks_to_archive_from,
+                blocks_to_archive_to,
+            );
+            for block_to_archive in blocks_to_archive_from..=blocks_to_archive_to {
+                let block = client
+                    .block(&BlockId::Number(block_to_archive.into()))
+                    .expect("Older block by number must always exist")
+                    .expect("Older block by number must always exist");
+
+                let block_object_mapping = client
+                    .runtime_api()
+                    .extract_block_object_mapping(
+                        &BlockId::Number(block_to_archive.saturating_sub(1).into()),
+                        block.block.clone(),
+                    )
+                    .expect("Must be able to make runtime call");
+
+                let encoded_block = block.encode();
+                debug!(
+                    target: "kumandra",
+                    "Encoded block {} has size of {:.2} kiB",
+                    block_to_archive,
+                    encoded_block.len() as f32 / 1024.0
+                );
+
+                // These archived segments were processed before, thus do not need to be sent to
+                // farmers
+                let new_root_blocks: Vec<RootBlock> = archiver
+                    .add_block(encoded_block, block_object_mapping)
+                    .into_iter()
+                    .map(|archived_segment| archived_segment.root_block)
+                    .collect();
+
+                if !new_root_blocks.is_empty() {
+                    // Set list of expected root blocks for the block where we expect root block
+                    // extrinsic to be included
+                    kumandra_link.root_blocks.lock().put(
+                        if block_to_archive.is_zero() {
+                            // Special case for genesis block whose root block should be included in
+                            // the first block in order for further validation to work properly.
+                            One::one()
+                        } else {
+                            (block_to_archive + confirmation_depth_k + 1).into()
+                        },
+                        new_root_blocks,
+                    );
+                }
+            }
+        }
+    }
+
+    spawner.spawn_essential_blocking(
+        "kumandra-archiver",
+        None,
+        Box::pin({
+            let mut imported_block_notification_stream =
+                kumandra_link.imported_block_notification_stream.subscribe();
+            let archived_segment_notification_sender =
+                kumandra_link.archived_segment_notification_sender.clone();
+
+            async move {
+                let mut last_archived_block_number =
+                    archiver.last_archived_block_number().map(Into::into);
+
+                while let Some((block_number, mut root_block_sender)) =
+                    imported_block_notification_stream.next().await
+                {
+                    let block_to_archive =
+                        match block_number.checked_sub(&confirmation_depth_k.into()) {
+                            Some(block_to_archive) => block_to_archive,
+                            None => {
+                                continue;
+                            }
+                        };
+
+                    if let Some(last_archived_block) = &mut last_archived_block_number {
+                        if *last_archived_block >= block_to_archive {
+                            // This block was already archived, skip
+                            continue;
+                        }
+
+                        *last_archived_block = block_to_archive;
+                    } else {
+                        last_archived_block_number.replace(block_to_archive);
+                    }
+
+                    debug!(target: "kumandra", "Archiving block {:?}", block_to_archive);
+
+                    let block = client
+                        .block(&BlockId::Number(block_to_archive))
+                        .expect("Older block by number must always exist")
+                        .expect("Older block by number must always exist");
+
+                    let block_object_mapping = client
+                        .runtime_api()
+                        .extract_block_object_mapping(
+                            &BlockId::Number(block_to_archive.saturating_sub(One::one())),
+                            block.block.clone(),
+                        )
+                        .expect("Must be able to make runtime call");
+
+                    let encoded_block = block.encode();
+                    debug!(
+                        target: "kumandra",
+                        "Encoded block {} has size of {:.2} kiB",
+                        block_to_archive,
+                        encoded_block.len() as f32 / 1024.0
+                    );
+                    for archived_segment in archiver.add_block(encoded_block, block_object_mapping)
+                    {
+                        let ArchivedSegment {
+                            root_block,
+                            pieces,
+                            object_mapping,
+                        } = archived_segment;
+
+                        archived_segment_notification_sender.notify(move || ArchivedSegment {
+                            root_block,
+                            pieces,
+                            object_mapping,
+                        });
+
+                        let _ = root_block_sender.send(root_block).await;
+                    }
+                }
+            }
+        }),
+    );
+}
---
+++ modules/consensus/src/verification.rs
@@ -0,0 +1,259 @@
+// Copyright (C) 2019-2021 Parity Technologies (UK) Ltd.
+// Copyright (C) 2021 Subspace Labs, Inc.
+// SPDX-License-Identifier: GPL-3.0-or-later WITH Classpath-exception-2.0
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+//! Verification for Kumandra headers.
+use super::{kumandra_err, BlockT, Error};
+use log::{debug, trace};
+use sc_consensus_slots::CheckedHeader;
+use schnorrkel::context::SigningContext;
+use sp_consensus_slots::Slot;
+use kp_consensus::digests::{CompatibleDigestItem, PreDigest};
+use kp_consensus::FarmerPublicKey;
+use sp_core::crypto::ByteArray;
+use sp_runtime::traits::Header;
+use sp_runtime::{DigestItem, RuntimeAppPublic};
+use kumandra_archiving::archiver;
+use kumandra_core_primitives::{PieceIndex, Randomness, Salt, Sha256Hash, Solution};
+use kumandra_solving::{
+    derive_global_challenge, is_local_challenge_valid, PieceDistance, KumandraCodec,
+};
+
+/// Kumandra verification parameters
+pub(super) struct VerificationParams<'a, B: 'a + BlockT> {
+    /// The header being verified.
+    pub(super) header: B::Header,
+    /// The pre-digest of the header being verified to avoid duplicated work.
+    pub(super) pre_digest: PreDigest<FarmerPublicKey>,
+    /// The slot number of the current time.
+    pub(super) slot_now: Slot,
+    /// Parameters for solution verification
+    pub(super) verify_solution_params: VerifySolutionParams<'a>,
+}
+
+/// Check a header has been signed by the right key. If the slot is too far in
+/// the future, an error will be returned. If successful, returns the pre-header
+/// and the digest item containing the seal.
+///
+/// The seal must be the last digest.  Otherwise, the whole header is considered
+/// unsigned.  This is required for security and must not be changed.
+///
+/// This digest item will always return `Some` when used with `as_kumandra_pre_digest`.
+pub(super) fn check_header<B: BlockT + Sized>(
+    params: VerificationParams<B>,
+) -> Result<CheckedHeader<B::Header, VerifiedHeaderInfo>, Error<B>> {
+    let VerificationParams {
+        mut header,
+        pre_digest,
+        slot_now,
+        verify_solution_params,
+    } = params;
+
+    trace!(target: "kumandra", "Checking header");
+    let seal = header
+        .digest_mut()
+        .pop()
+        .ok_or_else(|| kumandra_err(Error::HeaderUnsealed(header.hash())))?;
+
+    let sig = seal
+        .as_kumandra_seal()
+        .ok_or_else(|| kumandra_err(Error::HeaderBadSeal(header.hash())))?;
+
+    // The pre-hash of the header doesn't include the seal and that's what we sign
+    let pre_hash = header.hash();
+
+    if pre_digest.slot > slot_now {
+        header.digest_mut().push(seal);
+        return Ok(CheckedHeader::Deferred(header, pre_digest.slot));
+    }
+
+    debug!(
+        target: "kumandra",
+        "Verifying primary block #{} at slot: {}",
+        header.number(),
+        pre_digest.slot,
+    );
+
+    // Verify that block is signed properly
+    if !pre_digest.solution.public_key.verify(&pre_hash, &sig) {
+        return Err(kumandra_err(Error::BadSignature(pre_hash)));
+    }
+
+    // Verify that solution is valid
+    verify_solution(&pre_digest.solution, verify_solution_params)?;
+
+    Ok(CheckedHeader::Checked(
+        header,
+        VerifiedHeaderInfo { pre_digest, seal },
+    ))
+}
+
+pub(super) struct VerifiedHeaderInfo {
+    pub(super) pre_digest: PreDigest<FarmerPublicKey>,
+    pub(super) seal: DigestItem,
+}
+
+/// Check the solution signature validity.
+fn check_signature(
+    signing_context: &SigningContext,
+    solution: &Solution<FarmerPublicKey>,
+) -> Result<(), schnorrkel::SignatureError> {
+    let public_key = schnorrkel::PublicKey::from_bytes(solution.public_key.as_slice())?;
+    let signature = schnorrkel::Signature::from_bytes(&solution.signature)?;
+    public_key.verify(signing_context.bytes(&solution.tag), &signature)
+}
+
+/// Check if the tag of a solution's piece is valid.
+fn check_piece_tag<B: BlockT>(
+    slot: Slot,
+    salt: Salt,
+    solution: &Solution<FarmerPublicKey>,
+) -> Result<(), Error<B>> {
+    if !kumandra_solving::is_tag_valid(&solution.encoding, salt, solution.tag) {
+        return Err(Error::InvalidTag(slot));
+    }
+
+    Ok(())
+}
+
+/// Check piece validity.
+///
+/// If `records_root` is `None`, piece validity check will be skipped.
+pub(crate) fn check_piece<B: BlockT>(
+    slot: Slot,
+    records_root: Sha256Hash,
+    position: u64,
+    record_size: u32,
+    solution: &Solution<FarmerPublicKey>,
+) -> Result<(), Error<B>> {
+    let mut piece = solution.encoding;
+
+    // Ensure piece is decodable.
+    let kumandra_codec = KumandraCodec::new(&solution.public_key);
+    kumandra_codec
+        .decode(&mut piece, solution.piece_index)
+        .map_err(|_| Error::InvalidEncoding(slot))?;
+
+    if !archiver::is_piece_valid(
+        &piece,
+        records_root,
+        position as usize,
+        record_size as usize,
+    ) {
+        return Err(Error::InvalidEncoding(slot));
+    }
+
+    Ok(())
+}
+
+/// Returns true if `solution.tag` is within the solution range.
+fn is_within_solution_range(solution: &Solution<FarmerPublicKey>, solution_range: u64) -> bool {
+    let target = u64::from_be_bytes(solution.local_challenge.derive_target());
+    let (lower, is_lower_overflowed) = target.overflowing_sub(solution_range / 2);
+    let (upper, is_upper_overflowed) = target.overflowing_add(solution_range / 2);
+
+    let solution_tag = u64::from_be_bytes(solution.tag);
+
+    if is_lower_overflowed || is_upper_overflowed {
+        upper <= solution_tag || solution_tag <= lower
+    } else {
+        lower <= solution_tag && solution_tag <= upper
+    }
+}
+
+/// Returns true if piece index is within farmer sector
+fn is_within_max_plot(
+    piece_index: PieceIndex,
+    key: &FarmerPublicKey,
+    total_pieces: u64,
+    max_plot_size: u64,
+) -> bool {
+    if total_pieces < max_plot_size {
+        return true;
+    }
+    let max_distance = PieceDistance::MAX / total_pieces * max_plot_size;
+    PieceDistance::xor_distance(&piece_index.into(), key) <= max_distance
+}
+
+pub(crate) struct PieceCheckParams {
+    pub(crate) records_root: Sha256Hash,
+    pub(crate) position: u64,
+    pub(crate) record_size: u32,
+    pub(super) max_plot_size: u64,
+    pub(super) total_pieces: u64,
+}
+
+/// If `piece_check_params` is `None`, piece validity check will be skipped.
+pub(crate) struct VerifySolutionParams<'a> {
+    pub(crate) global_randomness: &'a Randomness,
+    pub(crate) solution_range: u64,
+    pub(crate) slot: Slot,
+    pub(crate) salt: Salt,
+    pub(crate) piece_check_params: Option<PieceCheckParams>,
+    pub(crate) signing_context: &'a SigningContext,
+}
+
+pub(crate) fn verify_solution<B: BlockT>(
+    solution: &Solution<FarmerPublicKey>,
+    params: VerifySolutionParams,
+) -> Result<(), Error<B>> {
+    let VerifySolutionParams {
+        global_randomness,
+        solution_range,
+        slot,
+        salt,
+        piece_check_params,
+        signing_context,
+    } = params;
+
+    if let Err(error) = is_local_challenge_valid(
+        derive_global_challenge(global_randomness, slot),
+        &solution.local_challenge,
+        &solution.public_key,
+    ) {
+        return Err(Error::BadLocalChallenge(slot, error));
+    }
+
+    if !is_within_solution_range(solution, solution_range) {
+        return Err(Error::OutsideOfSolutionRange(slot));
+    }
+
+    check_signature(signing_context, solution).map_err(|e| Error::BadSolutionSignature(slot, e))?;
+
+    check_piece_tag(slot, salt, solution)?;
+
+    if let Some(PieceCheckParams {
+        records_root,
+        position,
+        record_size,
+        max_plot_size,
+        total_pieces,
+    }) = piece_check_params
+    {
+        if !is_within_max_plot(
+            solution.piece_index,
+            &solution.public_key,
+            total_pieces,
+            max_plot_size,
+        ) {
+            return Err(Error::OutsideOfMaxPlot(slot));
+        }
+
+        check_piece(slot, records_root, position, record_size, solution)?;
+    }
+
+    Ok(())
+}
