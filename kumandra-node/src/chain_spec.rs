2d32575ad1908307c2ac84152e16bfb84520cce2
hangsiahong <honghangsia@gmail.com>
2022-05-02 18:03:16 +0700

    add kumandra node

---
+++ kumandra-node/src/chain_spec.rs
@@ -0,0 +1,298 @@
+// Copyright (C) 2021 Subspace Labs, Inc.
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+//! Subspace chain configurations.
+
+use frame_support::traits::Get;
+use sc_service::{ChainType, Properties};
+use sc_telemetry::TelemetryEndpoints;
+use sp_core::crypto::Ss58Codec;
+use sp_core::{sr25519, Pair, Public};
+use sp_executor::ExecutorId;
+use sp_runtime::traits::{IdentifyAccount, Verify};
+use kumandra_runtime::{
+    BalancesConfig, ExecutorConfig, GenesisConfig, SS58Prefix, SudoConfig, SystemConfig,
+    VestingConfig, DECIMAL_PLACES, MILLISECS_PER_BLOCK, SSC, WASM_BINARY,
+};
+use kumandra_runtime_primitives::{AccountId, Balance, BlockNumber, Signature};
+
+const POLKADOT_TELEMETRY_URL: &str = "wss://telemetry.polkadot.io/submit/";
+const SUBSPACE_TELEMETRY_URL: &str = "wss://telemetry.subspace.network/submit/";
+const TESTNET_CHAIN_SPEC: &[u8] = include_bytes!("../res/chain-spec-raw-snapshot-2022-mar-09.json");
+const TESTNET_BOOTSTRAP_NODE: &str = "/dns/farm-rpc.subspace.network/tcp/30333/p2p/12D3KooWPjMZuSYj35ehced2MTJFf95upwpHKgKUrFRfHwohzJXr";
+
+/// List of accounts which should receive token grants, amounts are specified in SSC.
+const TOKEN_GRANTS: &[(&str, u128)] = &[
+    (
+        "5Dns1SVEeDqnbSm2fVUqHJPCvQFXHVsgiw28uMBwmuaoKFYi",
+        3_000_000,
+    ),
+    (
+        "5DxtHHQL9JGapWCQARYUAWj4yDcwuhg9Hsk5AjhEzuzonVyE",
+        1_500_000,
+    ),
+    ("5EHhw9xuQNdwieUkNoucq2YcateoMVJQdN8EZtmRy3roQkVK", 133_333),
+    ("5C5qYYCQBnanGNPGwgmv6jiR2MxNPrGnWYLPFEyV1Xdy2P3x", 178_889),
+    ("5GBWVfJ253YWVPHzWDTos1nzYZpa9TemP7FpQT9RnxaFN6Sz", 350_000),
+    ("5F9tEPid88uAuGbjpyegwkrGdkXXtaQ9sGSWEnYrfVCUCsen", 111_111),
+    ("5DkJFCv3cTBsH5y1eFT94DXMxQ3EmVzYojEA88o56mmTKnMp", 244_444),
+    ("5G23o1yxWgVNQJuL4Y9UaCftAFvLuMPCRe7BCARxCohjoHc9", 311_111),
+    ("5GhHwuJoK1b7uUg5oi8qUXxWHdfgzv6P5CQSdJ3ffrnPRgKM", 317_378),
+    ("5EqBwtqrCV427xCtTsxnb9X2Qay39pYmKNk9wD9Kd62jLS97", 300_000),
+    ("5D9pNnGCiZ9UqhBQn5n71WFVaRLvZ7znsMvcZ7PHno4zsiYa", 600_000),
+    ("5DXfPcXUcP4BG8LBSkJDrfFNApxjWySR6ARfgh3v27hdYr5S", 430_000),
+    ("5CXSdDJgzRTj54f9raHN2Z5BNPSMa2ETjqCTUmpaw3ECmwm4", 330_000),
+    ("5DqKxL7bQregQmUfFgzTMfRKY4DSvA1KgHuurZWYmxYSCmjY", 200_000),
+    ("5CfixiS93yTwHQbzzfn8P2tMxhKXdTx7Jam9htsD7XtiMFtn", 27_800),
+    ("5FZe9YzXeEXe7sK5xLR8yCmbU8bPJDTZpNpNbToKvSJBUiEo", 18_067),
+    ("5FZwEgsvZz1vpeH7UsskmNmTpbfXvAcojjgVfShgbRqgC1nx", 27_800),
+];
+
+/// The `ChainSpec` parameterized for the subspace runtime.
+pub type SubspaceChainSpec = sc_service::GenericChainSpec<GenesisConfig>;
+
+/// Generate a crypto pair from seed.
+pub fn get_from_seed<TPublic: Public>(seed: &str) -> <TPublic::Pair as Pair>::Public {
+    TPublic::Pair::from_string(&format!("//{}", seed), None)
+        .expect("static values are valid; qed")
+        .public()
+}
+
+type AccountPublic = <Signature as Verify>::Signer;
+
+/// Generate an account ID from seed.
+pub fn get_account_id_from_seed(seed: &str) -> AccountId {
+    AccountPublic::from(get_from_seed::<sr25519::Public>(seed)).into_account()
+}
+
+pub fn testnet_config_json() -> Result<SubspaceChainSpec, String> {
+    SubspaceChainSpec::from_json_bytes(TESTNET_CHAIN_SPEC)
+}
+pub fn testnet_config_compiled() -> Result<SubspaceChainSpec, String> {
+    let mut properties = Properties::new();
+    properties.insert("ss58Format".into(), <SS58Prefix as Get<u16>>::get().into());
+    properties.insert("tokenDecimals".into(), DECIMAL_PLACES.into());
+    properties.insert("tokenSymbol".into(), "tSSC".into());
+
+    Ok(SubspaceChainSpec::from_genesis(
+        // Name
+        "Subspace testnet",
+        // ID
+        "kumandra_test",
+        ChainType::Custom("Subspace testnet".to_string()),
+        || {
+            let sudo_account =
+                AccountId::from_ss58check("5CXTmJEusve5ixyJufqHThmy4qUrrm6FyLCR7QfE4bbyMTNC")
+                    .expect("Wrong root account address");
+
+            let mut balances = vec![(sudo_account.clone(), 1_000 * SSC)];
+            let vesting_schedules = TOKEN_GRANTS
+                .iter()
+                .flat_map(|&(account_address, amount)| {
+                    let account_id = AccountId::from_ss58check(account_address)
+                        .expect("Wrong vesting account address");
+                    let amount: Balance = amount * SSC;
+
+                    // TODO: Adjust start block to real value before mainnet launch
+                    let start_block = 100_000_000;
+                    let one_month_in_blocks =
+                        u32::try_from(3600 * 24 * 30 * MILLISECS_PER_BLOCK / 1000)
+                            .expect("One month of blocks always fits in u32; qed");
+
+                    // Add balance so it can be locked
+                    balances.push((account_id.clone(), amount));
+
+                    [
+                        // 1/4 of tokens are released after 1 year.
+                        (
+                            account_id.clone(),
+                            start_block,
+                            one_month_in_blocks * 12,
+                            1,
+                            amount / 4,
+                        ),
+                        // 1/48 of tokens are released every month after that for 3 more years.
+                        (
+                            account_id,
+                            start_block + one_month_in_blocks * 12,
+                            one_month_in_blocks,
+                            36,
+                            amount / 48,
+                        ),
+                    ]
+                })
+                .collect::<Vec<_>>();
+            kumandra_genesis_config(
+                WASM_BINARY.expect("Wasm binary must be built for testnet"),
+                sudo_account,
+                balances,
+                vesting_schedules,
+                (
+                    get_account_id_from_seed("Alice"),
+                    get_from_seed::<ExecutorId>("Alice"),
+                ),
+            )
+        },
+        // Bootnodes
+        vec![TESTNET_BOOTSTRAP_NODE
+            .parse()
+            .expect("Bootstrap node must be correct")],
+        // Telemetry
+        Some(
+            TelemetryEndpoints::new(vec![
+                (POLKADOT_TELEMETRY_URL.into(), 1),
+                (SUBSPACE_TELEMETRY_URL.into(), 1),
+            ])
+            .map_err(|error| error.to_string())?,
+        ),
+        // Protocol ID
+        Some("subspace-substrate"),
+        None,
+        // Properties
+        Some(properties),
+        // Extensions
+        None,
+    ))
+}
+
+pub fn dev_config() -> Result<SubspaceChainSpec, String> {
+    let mut properties = Properties::new();
+    properties.insert("ss58Format".into(), <SS58Prefix as Get<u16>>::get().into());
+    properties.insert("tokenDecimals".into(), DECIMAL_PLACES.into());
+    properties.insert("tokenSymbol".into(), "tSSC".into());
+
+    let wasm_binary = WASM_BINARY.ok_or_else(|| "Development wasm not available".to_string())?;
+
+    Ok(SubspaceChainSpec::from_genesis(
+        // Name
+        "Subspace development",
+        // ID
+        "kumandra_dev",
+        ChainType::Development,
+        || {
+            kumandra_genesis_config(
+                wasm_binary,
+                // Sudo account
+                get_account_id_from_seed("Alice"),
+                // Pre-funded accounts
+                vec![
+                    (get_account_id_from_seed("Alice"), 1_000 * SSC),
+                    (get_account_id_from_seed("Bob"), 1_000 * SSC),
+                    (get_account_id_from_seed("Alice//stash"), 1_000 * SSC),
+                    (get_account_id_from_seed("Bob//stash"), 1_000 * SSC),
+                ],
+                vec![],
+                (
+                    get_account_id_from_seed("Alice"),
+                    get_from_seed::<ExecutorId>("Alice"),
+                ),
+            )
+        },
+        // Bootnodes
+        vec![],
+        // Telemetry
+        None,
+        // Protocol ID
+        None,
+        None,
+        // Properties
+        Some(properties),
+        // Extensions
+        None,
+    ))
+}
+
+pub fn local_config() -> Result<SubspaceChainSpec, String> {
+    let mut properties = Properties::new();
+    properties.insert("ss58Format".into(), <SS58Prefix as Get<u16>>::get().into());
+    properties.insert("tokenDecimals".into(), DECIMAL_PLACES.into());
+    properties.insert("tokenSymbol".into(), "tSSC".into());
+
+    let wasm_binary = WASM_BINARY.ok_or_else(|| "Development wasm not available".to_string())?;
+
+    Ok(SubspaceChainSpec::from_genesis(
+        // Name
+        "Subspace local",
+        // ID
+        "kumandra_local",
+        ChainType::Local,
+        || {
+            kumandra_genesis_config(
+                wasm_binary,
+                // Sudo account
+                get_account_id_from_seed("Alice"),
+                // Pre-funded accounts
+                vec![
+                    (get_account_id_from_seed("Alice"), 1_000 * SSC),
+                    (get_account_id_from_seed("Bob"), 1_000 * SSC),
+                    (get_account_id_from_seed("Charlie"), 1_000 * SSC),
+                    (get_account_id_from_seed("Dave"), 1_000 * SSC),
+                    (get_account_id_from_seed("Eve"), 1_000 * SSC),
+                    (get_account_id_from_seed("Ferdie"), 1_000 * SSC),
+                    (get_account_id_from_seed("Alice//stash"), 1_000 * SSC),
+                    (get_account_id_from_seed("Bob//stash"), 1_000 * SSC),
+                    (get_account_id_from_seed("Charlie//stash"), 1_000 * SSC),
+                    (get_account_id_from_seed("Dave//stash"), 1_000 * SSC),
+                    (get_account_id_from_seed("Eve//stash"), 1_000 * SSC),
+                    (get_account_id_from_seed("Ferdie//stash"), 1_000 * SSC),
+                ],
+                vec![],
+                (
+                    get_account_id_from_seed("Alice"),
+                    get_from_seed::<ExecutorId>("Alice"),
+                ),
+            )
+        },
+        // Bootnodes
+        vec![],
+        // Telemetry
+        None,
+        // Protocol ID
+        None,
+        None,
+        // Properties
+        Some(properties),
+        // Extensions
+        None,
+    ))
+}
+
+/// Configure initial storage state for FRAME modules.
+fn kumandra_genesis_config(
+    wasm_binary: &[u8],
+    sudo_account: AccountId,
+    balances: Vec<(AccountId, Balance)>,
+    // who, start, period, period_count, per_period
+    vesting: Vec<(AccountId, BlockNumber, BlockNumber, u32, Balance)>,
+    executor_authority: (AccountId, ExecutorId),
+) -> GenesisConfig {
+    GenesisConfig {
+        system: SystemConfig {
+            // Add Wasm runtime to storage.
+            code: wasm_binary.to_vec(),
+        },
+        balances: BalancesConfig { balances },
+        transaction_payment: Default::default(),
+        sudo: SudoConfig {
+            // Assign network admin rights.
+            key: Some(sudo_account),
+        },
+        vesting: VestingConfig { vesting },
+        executor: ExecutorConfig {
+            executor: Some(executor_authority),
+        },
+    }
+}
---
+++ kumandra-node/src/lib.rs
@@ -0,0 +1,173 @@
+// Copyright (C) 2021 Subspace Labs, Inc.
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+//! Subspace Node library.
+
+mod chain_spec;
+mod import_blocks_from_dsn;
+mod secondary_chain_cli;
+
+pub use crate::import_blocks_from_dsn::ImportBlocksFromDsnCmd;
+pub use crate::secondary_chain_cli::SecondaryChainCli;
+use clap::Parser;
+use sc_cli::SubstrateCli;
+use sc_executor::{NativeExecutionDispatch, RuntimeVersion};
+use sc_service::ChainSpec;
+
+/// Executor dispatch for subspace runtime
+pub struct ExecutorDispatch;
+
+impl NativeExecutionDispatch for ExecutorDispatch {
+    /// Only enable the benchmarking host functions when we actually want to benchmark.
+    #[cfg(feature = "runtime-benchmarks")]
+    type ExtendHostFunctions = (
+        sp_executor::fraud_proof_ext::fraud_proof::HostFunctions,
+        frame_benchmarking::benchmarking::HostFunctions,
+    );
+    /// Otherwise we only use the default Substrate host functions.
+    #[cfg(not(feature = "runtime-benchmarks"))]
+    type ExtendHostFunctions = sp_executor::fraud_proof_ext::fraud_proof::HostFunctions;
+
+    fn dispatch(method: &str, data: &[u8]) -> Option<Vec<u8>> {
+        kumandra_runtime::api::dispatch(method, data)
+    }
+
+    fn native_version() -> sc_executor::NativeVersion {
+        kumandra_runtime::native_version()
+    }
+}
+
+/// Utilities for working with a node.
+#[derive(Debug, clap::Subcommand)]
+pub enum Subcommand {
+    /// Key management cli utilities
+    #[clap(subcommand)]
+    Key(sc_cli::KeySubcommand),
+
+    /// Build a chain specification.
+    BuildSpec(sc_cli::BuildSpecCmd),
+
+    /// Validate blocks.
+    CheckBlock(sc_cli::CheckBlockCmd),
+
+    /// Export blocks.
+    ExportBlocks(sc_cli::ExportBlocksCmd),
+
+    /// Export the state of a given block into a chain spec.
+    ExportState(sc_cli::ExportStateCmd),
+
+    /// Import blocks.
+    ImportBlocks(sc_cli::ImportBlocksCmd),
+
+    /// Import blocks from Subspace Network DSN.
+    ImportBlocksFromDsn(ImportBlocksFromDsnCmd),
+
+    /// Remove the whole chain.
+    PurgeChain(sc_cli::PurgeChainCmd),
+
+    /// Revert the chain to a previous state.
+    Revert(sc_cli::RevertCmd),
+
+    /// Run executor sub-commands.
+    #[clap(subcommand)]
+    Executor(cirrus_node::cli::Subcommand),
+
+    /// Sub-commands concerned with benchmarking.
+    #[clap(subcommand)]
+    Benchmark(frame_benchmarking_cli::BenchmarkCmd),
+}
+
+/// Command used to run a Subspace node.
+#[derive(Debug, Parser)]
+pub struct RunCmd {
+    /// Base command to run a node.
+    #[clap(flatten)]
+    pub base: sc_cli::RunCmd,
+}
+
+/// Subspace Cli.
+#[derive(Debug, Parser)]
+#[clap(
+    propagate_version = true,
+    args_conflicts_with_subcommands = true,
+    subcommand_negates_reqs = true
+)]
+pub struct Cli {
+    /// Various utility commands.
+    #[clap(subcommand)]
+    pub subcommand: Option<Subcommand>,
+
+    /// Run a node.
+    #[clap(flatten)]
+    pub run: RunCmd,
+
+    /// Secondary chain arguments
+    ///
+    /// The command-line arguments provided first will be passed to the embedded primary node,
+    /// while the arguments provided after -- will be passed to the executor node.
+    ///
+    /// subspace-node [primarychain-args] -- [secondarychain-args]
+    #[clap(raw = true)]
+    pub secondary_chain_args: Vec<String>,
+}
+
+impl SubstrateCli for Cli {
+    fn impl_name() -> String {
+        "Subspace".into()
+    }
+
+    fn impl_version() -> String {
+        env!("SUBSTRATE_CLI_IMPL_VERSION").into()
+    }
+
+    fn executable_name() -> String {
+        // Customize to make sure directory used for data by default is the same regardless of the
+        // name of the executable file.
+        "subspace-node".to_string()
+    }
+
+    fn description() -> String {
+        env!("CARGO_PKG_DESCRIPTION").into()
+    }
+
+    fn author() -> String {
+        env!("CARGO_PKG_AUTHORS").into()
+    }
+
+    fn support_url() -> String {
+        "https://discord.gg/vhKF9w3x".into()
+    }
+
+    fn copyright_start_year() -> i32 {
+        2021
+    }
+
+    fn load_spec(&self, id: &str) -> Result<Box<dyn ChainSpec>, String> {
+        Ok(match id {
+            "testnet" => Box::new(chain_spec::testnet_config_json()?),
+            "testnet-compiled" => Box::new(chain_spec::testnet_config_compiled()?),
+            "dev" => Box::new(chain_spec::dev_config()?),
+            "" | "local" => Box::new(chain_spec::local_config()?),
+            path => Box::new(chain_spec::SubspaceChainSpec::from_json_file(
+                std::path::PathBuf::from(path),
+            )?),
+        })
+    }
+
+    fn native_runtime_version(_: &Box<dyn ChainSpec>) -> &'static RuntimeVersion {
+        &kumandra_runtime::VERSION
+    }
+}
