fce76c5470448a57604d3a03420579437a75b1cf
hangsiahong <honghangsia@gmail.com>
2022-05-04 09:47:57 +0700

    update

--- utils/wasm-tools/src/lib.rs
+++ utils/wasm-tools/src/lib.rs
@@ -19,7 +19,7 @@ pub fn create_runtime_bundle_inclusion_file(
 ) {
     let out_dir = PathBuf::from(env::var("OUT_DIR").expect("Always set by cargo"));
     let target_dir = env::var("CARGO_TARGET_DIR")
-        .or_else(|_| env::var("SUBSPACE_WASM_TARGET_DIRECTORY"))
+        .or_else(|_| env::var("KUMANDRA_WASM_TARGET_DIRECTORY"))
         .map(Into::into)
         .unwrap_or_else(|_| {
             // Call get `get_relative_target` twice if possible, as wasm target directory is
@@ -30,14 +30,14 @@ pub fn create_runtime_bundle_inclusion_file(
         });
 
     // Propagate target directory to wasm build
-    if env::var("SUBSPACE_WASM_TARGET_DIRECTORY").is_err() {
-        env::set_var("SUBSPACE_WASM_TARGET_DIRECTORY", &target_dir);
+    if env::var("KUMANDRA_WASM_TARGET_DIRECTORY").is_err() {
+        env::set_var("KUMANDRA_WASM_TARGET_DIRECTORY", &target_dir);
     }
 
     // Make correct profile accessible in child processes.
     env::set_var(
-        "SUBSPACE_WASM_BUILD_PROFILE",
-        env::var("SUBSPACE_WASM_BUILD_PROFILE").unwrap_or_else(|_| {
+        "KUMANDRA_WASM_BUILD_PROFILE",
+        env::var("KUMANDRA_WASM_BUILD_PROFILE").unwrap_or_else(|_| {
             // The tricky situation, Cargo doesn't put custom profiles like `production` in
             // `PROFILE` environment variable, so we need to find the actual profile ourselves by
             // extracting the first component of `OUT_DIR` that comes after `CARGO_TARGET_DIR`.
@@ -62,7 +62,7 @@ pub fn create_runtime_bundle_inclusion_file(
     );
 
     // Create a file that will include execution runtime into consensus runtime
-    let profile = env::var("SUBSPACE_WASM_BUILD_PROFILE").expect("Set above; qed");
+    let profile = env::var("KUMANDRA_WASM_BUILD_PROFILE").expect("Set above; qed");
     let execution_wasm_bundle_path = target_dir
         .join(profile)
         .join("wbuild")
